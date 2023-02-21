9f40af74901f485a7db094df0870c546e583a9a4
JesusUribe3 <chucho.jo33@gmail.com>
2023-02-20 21:29:14 -0500

    se crean variantes
    se crean variantes de los lados y se convierten los datos

--- Clase_22_feb_Dispositivos/app/src/main/java/jesus/uribe/dispositivos/clase3febrero2023/MainActivity.java
+++ Clase_22_feb_Dispositivos/app/src/main/java/jesus/uribe/dispositivos/clase3febrero2023/MainActivity.java
@@ -3,14 +3,15 @@ package jesus.uribe.dispositivos.clase3febrero2023;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.ed
+
 
 public class MainActivity extends AppCompatActivity {
  String TAG = "Depuracion";
- EditText lado1, ladodo2;
+ EditText ladouno, ladodos;
  TextView resultado;
  Button calcular;
 
@@ -19,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "Estoy en onCreate");
-        lado1 =  findViewById(R.id.lado1);
-        ladodo2 = findViewById(R.id.lado2);
+        ladouno =  findViewById(R.id.lado1);
+        ladodos = findViewById(R.id.lado2);
         resultado = findViewById(R.id.resultado);
         calcular = findViewById(R.id.calcular);
 
@@ -30,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "Estoy en OnStart");
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float ladoone = Float.parseFloat(ladouno.getText().toString());
+                float ladotwo = Float.parseFloat(ladodos.getText().toString());
+                float result =ladoone*ladotwo;
+                resultado.setText(String.valueOf(result));
+
+            }
+        });
 
     }
 
